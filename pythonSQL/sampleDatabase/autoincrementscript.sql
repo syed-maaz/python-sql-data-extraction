af7aaddc676aaf4a86fae47ba7509ee6460a2ad7
Irtiza Yawar <irtiza.yawar@jamipartners.com>
2022-09-02 15:24:25 +0500

    API 1st commit

---
+++ pythonSQL/sampleDatabase/autoincrementscript.sql
@@ -0,0 +1,11 @@
+SELECT * FROM production.productsdata;
+-- Alter TABLE productsdata
+-- use production;
+-- SHOW KEYS FROM productsdata WHERE Key_name = 'PRIMARY'
+-- describe productsdata;
+-- ALTER TABLE productsdata
+-- ADD PRIMARY KEY (product_id);
+
+-- ALTER TABLE productsdata CHANGE product_id product_id INT AUTO_INCREMENT;
+-- ALTER table productsdata AUTO_INCREMENT = 100;-- 
+describe productsdata
